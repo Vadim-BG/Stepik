94daee44307c4446bcc8b5aba027a88183cf7b71
Vadim-BG <infotourbg.vadim@gmail.com>
2020-05-19 11:16:23 +0300

    adaptive lessons Stepik

---
+++ src/adaptiveStepik/SetForStepik.java
@@ -0,0 +1,13 @@
+package adaptiveStepik;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class SetForStepik {
+    public static void main(String[] args) {
+        String[] stringArray = {"Alpha", "Gamma", "Omega"};
+        ArrayList<String> set = new ArrayList<>(Arrays.asList(stringArray));
+            System.out.println(set);
+    }
+}
